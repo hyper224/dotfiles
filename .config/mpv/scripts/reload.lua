7fbe310b0883d893f217be55720df4fbae1b2e21
hyper224 <loothy673@gmail.com>
2022-04-20 00:53:11 +0000

    Remove mpv config

--- .config/mpv/scripts/reload.lua
+++
@@ -1,9 +0,0 @@
-function reload()
-    local path = mp.get_property("path")
-    if path ~= nil then
-        local time = mp.get_property_number("time-pos")
-        mp.commandv("loadfile", path, "replace", "start=" .. time)
-    end
-end
-
-mp.add_key_binding("R", "reload", reload)
